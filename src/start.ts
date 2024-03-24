8bae73b0e149d40e691435f6e3b1c6453aec78d4
dylandray <120407558+dylandray@users.noreply.github.com>
2024-03-24 21:41:33 +0100

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -2,7 +2,6 @@ import { launchNetwork } from ".";
 import { startConsensus } from "./nodes/consensus";
 import { Value } from "./types";
 import { delay } from "./utils";
-
 async function main() {
   const faultyArray = [
     true,
@@ -16,14 +15,14 @@ async function main() {
     false,
     false,
   ];
-  const initialValues: Value[] = [1, 0, 1, 0, 1, 0, 1, 0, 1, 1];
+  const initialValues: Value[] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   if (initialValues.length !== faultyArray.length)
-    throw new Error("Lengths don't match");
+    throw new Error("Lengths do not match");
   if (
     faultyArray.filter((faulty) => faulty === true).length >
     initialValues.length / 2
   )
-    throw new Error("Too many faulty nodes");
+    throw new Error("Too much faulty nodes");
   await launchNetwork(
     initialValues.length,
     faultyArray.filter((el) => el === true).length,
@@ -31,7 +30,6 @@ async function main() {
     faultyArray
   );
   await delay(200);
-
   await startConsensus(initialValues.length);
 }
 main();
